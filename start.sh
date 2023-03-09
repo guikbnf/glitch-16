e08d8e57e02771fb6a9c901c753ca6feaeb1a412
guikbnf <116950566+guikbnf@users.noreply.github.com>
2023-03-08 18:16:34 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # set val
 python3 /app/self-ping.py &
 
-AUUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
+AUUID=2350f5af-6817-4e10-ab15-2015989f1d4d
 ParameterSSENCYPT=chacha20-ietf-poly1305
 CADDYIndexPage=https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html
 
